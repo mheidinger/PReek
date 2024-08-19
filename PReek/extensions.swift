c33266ef2926ea12a9001da8a491819105c7cb8a
Max Heidinger <mail@max-heidinger.de>
2024-08-19 14:39:02 +0200

    decrease opacity of window, darken additions/deletions colors in light mode

--- PReek/extensions.swift
+++ PReek/extensions.swift
@@ -33,3 +33,14 @@ struct CodableAppStorage<T: Codable> {
         }
     }
 }
+
+// Add `if` to view builder that only applies modifiers if the condition is true
+extension View {
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
