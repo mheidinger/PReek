a4dfae060775605bc41ed5d1c5d17d92c1f2197b
Max Heidinger <mail@max-heidinger.de>
2024-11-21 10:50:39 +0100

    fix: memoize filtered/sorted pull requests

--- PReek/ViewModel/PullRequestsViewModel.swift
+++ PReek/ViewModel/PullRequestsViewModel.swift
@@ -1,46 +1,95 @@
+import Combine
 import OSLog
 import SwiftUI
 
 class PullRequestsViewModel: ObservableObject {
-    @Published var lastUpdated: Date? = nil
-    @Published var isRefreshing = false
-    @Published var error: Error? = nil
-
     private let logger = Logger()
 
-    @AppStorage("hideClosed") var hideClosed: Bool = true {
+    @Published private(set) var lastUpdated: Date? = nil
+    @Published private(set) var isRefreshing = false
+    @Published private(set) var error: Error? = nil
+    @Published private(set) var hasUnread: Bool = false
+
+    private var hideClosedSubject: CurrentValueSubject<Bool, Never>
+    @AppStorage("hideClosed") var hideClosed: Bool = false {
         didSet {
-            objectWillChange.send()
+            hideClosedSubject.send(hideClosed)
         }
     }
 
-    @AppStorage("hideRead") var hideRead: Bool = true {
+    private var hideReadSubject = CurrentValueSubject<Bool, Never>(false)
+    @AppStorage("hideRead") var hideRead: Bool = false {
         didSet {
-            objectWillChange.send()
+            hideReadSubject.send(hideRead)
         }
+    }
+
+    var pullRequests: [PullRequest] {
+        memoizedPullRequests
+    }
+
+    init(initialPullRequests: [PullRequest] = []) {
+        pullRequestMap = Dictionary(uniqueKeysWithValues: initialPullRequests.map { ($0.id, $0) })
+
+        // Directly access UserDefaults w/ same keys above for correct initial values in the subject
+        let storedHideClosed = UserDefaults.standard.bool(forKey: "hideClosed")
+        let storedHideRead = UserDefaults.standard.bool(forKey: "hideRead")
+        hideClosedSubject = CurrentValueSubject<Bool, Never>(storedHideClosed)
+        hideReadSubject = CurrentValueSubject<Bool, Never>(storedHideRead)
+        setupPullRequestsMemoization()
     }
 
     @CodableAppStorage("pullRequestReadMap") private var pullRequestReadMap: [String: Date] = [:]
-    @Published var hasUnread: Bool = false
+    private var pullRequestMap: [String: PullRequest] = [:]
+    @Published private var memoizedPullRequests: [PullRequest] = []
+    private var cancellables = Set<AnyCancellable>()
+    private let invalidationTrigger = PassthroughSubject<Void, Never>()
 
-    @Published private var pullRequestMap: [String: PullRequest] = [:]
-    var pullRequests: [PullRequest] {
-        pullRequestMap.map { entry in
-            var pullRequest = entry.value
-            pullRequest.lastMarkedAsRead = pullRequestReadMap[pullRequest.id]
-            return pullRequest
-        }.filter { pullRequest in
-            let containsNonExcludedUser = pullRequest.events.contains { event in
-                !ConfigService.excludedUsers.contains(event.user.login)
+    private func setupPullRequestsMemoization() {
+        // Combine dependencies that affect the pullRequests computation
+        // Also add last updated as excluded users are not considered
+        Publishers.CombineLatest4(
+            hideClosedSubject,
+            hideReadSubject,
+            $lastUpdated,
+            invalidationTrigger
+                .prepend(())
+                .setFailureType(to: Never.self)
+        )
+        .throttle(for: .milliseconds(100), scheduler: DispatchQueue.main, latest: true)
+        .map { [weak self] hideClosed, hideRead, _, _ in
+            guard let self = self else { return ([], false) }
+
+            logger.info("Memoize pull requests")
+
+            let updatedRead = self.pullRequestMap.map { entry in
+                var pullRequest = entry.value
+                pullRequest.lastMarkedAsRead = self.pullRequestReadMap[pullRequest.id]
+                return pullRequest
+            }
+            let filtered = updatedRead.filter { pullRequest in
+                let containsNonExcludedUser = pullRequest.events.contains { event in
+                    !ConfigService.excludedUsers.contains(event.user.login)
+                }
+
+                let passesClosedFilter = !hideClosed || !pullRequest.isClosed
+                let passesReadFilter = !hideRead || pullRequest.unread
+
+                return containsNonExcludedUser && passesClosedFilter && passesReadFilter
+            }
+            let filteredAndSorted = filtered.sorted {
+                $0.lastUpdated > $1.lastUpdated
             }
 
-            let passesClosedFilter = !hideClosed || !pullRequest.isClosed
-            let passesReadFilter = !hideRead || pullRequest.unread
-
-            return containsNonExcludedUser && passesClosedFilter && passesReadFilter
-        }.sorted {
-            $0.lastUpdated > $1.lastUpdated
+            let hasUnread = filteredAndSorted.contains { $0.unread }
+            return (filteredAndSorted, hasUnread)
         }
+        .receive(on: DispatchQueue.main)
+        .sink { [weak self] (pullRequests: [PullRequest], hasUnread: Bool) in
+            self?.memoizedPullRequests = pullRequests
+            self?.hasUnread = hasUnread
+        }
+        .store(in: &cancellables)
     }
 
     private var timer: Timer?
@@ -66,20 +115,14 @@ class PullRequestsViewModel: ObservableObject {
         } else {
             pullRequestReadMap.removeValue(forKey: pullRequest.id)
         }
-        updateHasUnread()
-        objectWillChange.send()
+        invalidationTrigger.send()
     }
 
     func markAllAsRead() {
         for pullRequest in pullRequests {
             pullRequestReadMap[pullRequest.id] = lastUpdated
         }
-        updateHasUnread()
-        objectWillChange.send()
-    }
-
-    private func updateHasUnread() {
-        hasUnread = pullRequests.first { pullRequest in pullRequest.unread } != nil
+        invalidationTrigger.send()
     }
 
     private func handleReceivedNotifications(notifications: [Notification], viewer: Viewer) async throws -> [String] {
@@ -102,11 +145,11 @@ class PullRequestsViewModel: ObservableObject {
             let pullRequests = try await GitHubService.fetchPullRequests(repoMap: repoMap, viewer: viewer)
             logger.info("Got \(pullRequests.count) pull requests")
 
-            DispatchQueue.main.async {
+            await MainActor.run {
                 for pullRequest in pullRequests {
                     self.pullRequestMap[pullRequest.id] = pullRequest
                 }
-                self.updateHasUnread()
+                self.invalidationTrigger.send()
             }
             return pullRequests.map { $0.id }
         } else {
@@ -117,7 +160,7 @@ class PullRequestsViewModel: ObservableObject {
 
     func updatePullRequests() async {
         do {
-            DispatchQueue.main.async {
+            await MainActor.run {
                 self.isRefreshing = true
             }
 
@@ -142,24 +185,24 @@ class PullRequestsViewModel: ObservableObject {
             }
             _ = try await fetchPullRequestMap(repoMap: notUpdatedRepoMap, viewer: viewer)
 
-            cleanupPullRequests()
-
-            DispatchQueue.main.async {
+            await MainActor.run {
                 self.lastUpdated = newLastUpdated
                 self.isRefreshing = false
                 self.error = nil
             }
+
+            await cleanupPullRequests()
             logger.info("Finished fetching notifications")
         } catch {
             logger.error("Failed to get pull requests: \(error)")
-            DispatchQueue.main.async {
+            await MainActor.run {
                 self.isRefreshing = false
                 self.error = error
             }
         }
     }
 
-    private func cleanupPullRequests() {
+    private func cleanupPullRequests() async {
         let daysToDeduct = (ConfigService.deleteAfterWeeks * 7) + 1
         let deleteFrom = Calendar.current.date(byAdding: .day, value: daysToDeduct * -1, to: Date())!
 
@@ -176,7 +219,7 @@ class PullRequestsViewModel: ObservableObject {
             logger.info("Removing \(self.pullRequestMap.count - filteredPullRequestMap.count) pull requests")
             logger.info("Removing \(self.pullRequestReadMap.count - filteredPullRequestReadMap.count) pull requests read info")
             // swiftformat:enable redundantSelf
-            DispatchQueue.main.async {
+            await MainActor.run {
                 self.pullRequestMap = filteredPullRequestMap
                 self.pullRequestReadMap = filteredPullRequestReadMap
             }
