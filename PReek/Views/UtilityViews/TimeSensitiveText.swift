cb5f376f452f9a3b2cc4352f0466c36942e9c03e
Max Heidinger <mail@max-heidinger.de>
2025-10-14 14:44:32 +0200

    fix: optimize time sensitive text

--- PReek/Views/UtilityViews/TimeSensitiveText.swift
+++ PReek/Views/UtilityViews/TimeSensitiveText.swift
@@ -1,24 +1,34 @@
 import SwiftUI
 
+private class TimeSensitiveTextTimer: ObservableObject {
+    static let shared = TimeSensitiveTextTimer()
+
+    @Published private(set) var tick = Date()
+
+    private init() {
+        Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { _ in
+            self.tick = Date()
+        }
+    }
+}
+
 struct TimeSensitiveText: View {
     let getText: () -> String
     @State private var currentText: String
+    @ObservedObject private var timer = TimeSensitiveTextTimer.shared
 
     init(getText: @escaping () -> String) {
         self.getText = getText
-        currentText = getText()
+        self._currentText = State(initialValue: getText())
     }
-
-    private let timer = Timer.publish(
-        every: 30,
-        on: .main,
-        in: .common
-    ).autoconnect()
 
     var body: some View {
         Text(currentText)
-            .onReceive(timer) { _ in
-                currentText = getText()
+            .onReceive(timer.$tick) { _ in
+                let newText = getText()
+                if newText != currentText {
+                    currentText = newText
+                }
             }
     }
 }
